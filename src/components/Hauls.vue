a6c6faffdebc33d17353128bc52133fc3283bd62
Juan Sebastian Benitez Ospina <jbenitez@hugeinc.com>
2020-12-10 11:29:36 -0500

    Create Accessorials component

--- src/components/Hauls.vue
+++ src/components/Hauls.vue
@@ -4,16 +4,21 @@
       <Track title="Pickup" zip="Zip from" city="City from" type="pickup" />
       <Track title="Delivery" zip="Zip to" city="City to" type="delivery" />
     </div>
+    <div class="hauls__accessorials">
+      <Accessorials />
+    </div>
   </div>
 </template>
 
 <script>
 import Track from "@/components/Track";
+import Accessorials from "@/components/Accessorials";
 
 export default {
   name: "Hauls",
   components: {
-    Track
+    Track,
+    Accessorials
   }
 };
 </script>
@@ -21,11 +26,13 @@ export default {
 <style scoped lang="scss">
 .hauls {
   background-color: #b6b6b6;
+  padding: 10px 0;
   width: 100vw;
 
   &__track {
     display: flex;
-    padding: 10px 20px;
+    margin-bottom: 20px;
+    padding: 0 20px;
 
     .track {
       flex-basis: 50%;
@@ -34,6 +41,10 @@ export default {
         margin-right: 30px;
       }
     }
+  }
+
+  &__accessorials {
+    padding: 0 20px;
   }
 }
 </style>
